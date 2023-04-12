df3589daf6ea79ae71ae50aa6e17d53025e93279
0xSzeth <0xszeth@gmail.com>
2023-04-12 14:27:38 -0600

    chore: update LiquidityGaugeFactory mapping

--- src/mappings/LiquidityGaugeFactory.ts
+++ src/mappings/LiquidityGaugeFactory.ts
@@ -2,7 +2,7 @@ import { Bytes, crypto } from "@graphprotocol/graph-ts";
 import { GaugeCreated } from "../../generated/LiquidityGaugeFactory/LiquidityGaugeFactory";
 import { LiquidityGauge } from "../../generated/LiquidityGaugeFactory/LiquidityGauge";
 import { BunniToken } from "../../generated/LiquidityGaugeFactory/BunniToken";
-import { getBunniToken, getGauge } from "../utils/entities";
+import { getGauge } from "../utils/entities";
 import { bunniKey } from "../utils/helpers";
 
 export function handleGaugeCreated(event: GaugeCreated): void {
@@ -18,8 +18,4 @@ export function handleGaugeCreated(event: GaugeCreated): void {
   gauge.address = event.params.gauge;
   gauge.bunniToken = bunniKey(pool, tickLower, tickUpper);
   gauge.save();
-
-  let bunniToken = getBunniToken(gauge.bunniToken);
-  bunniToken.gauge = event.params.gauge;
-  bunniToken.save();
 }
