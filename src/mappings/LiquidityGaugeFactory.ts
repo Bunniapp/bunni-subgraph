a1b8240267e737f5b1361c5f03b577a05487ecdb
0xSzeth <0xszeth@gmail.com>
2023-05-01 12:23:22 -0600

    feat: handle child gauge creation

--- src/mappings/LiquidityGaugeFactory.ts
+++ src/mappings/LiquidityGaugeFactory.ts
@@ -1,6 +1,8 @@
 import { Bytes, crypto } from "@graphprotocol/graph-ts";
 import { GaugeCreated } from "../../generated/LiquidityGaugeFactory/LiquidityGaugeFactory";
 import { LiquidityGauge } from "../../generated/LiquidityGaugeFactory/LiquidityGauge";
+import { DeployedGauge } from "../../generated/ChildGaugeFactory/ChildGaugeFactory";
+import { ChildGauge } from "../../generated/ChildGaugeFactory/ChildGauge";
 import { BunniToken } from "../../generated/LiquidityGaugeFactory/BunniToken";
 import { getGauge } from "../utils/entities";
 import { bunniKey } from "../utils/helpers";
@@ -16,6 +18,21 @@ export function handleGaugeCreated(event: GaugeCreated): void {
 
   let gauge = getGauge(Bytes.fromByteArray(crypto.keccak256(event.params.gauge)));
   gauge.address = event.params.gauge;
+  gauge.bunniToken = bunniKey(pool, tickLower, tickUpper);
+  gauge.save();
+}
+
+export function handleDeployedGauge(event: DeployedGauge): void {
+  let childGauge = ChildGauge.bind(event.params._gauge);
+  let lpToken = childGauge.lp_token();
+
+  let bunniTokenContract = BunniToken.bind(lpToken);
+  let pool = bunniTokenContract.pool();
+  let tickLower = bunniTokenContract.tickLower();
+  let tickUpper = bunniTokenContract.tickUpper();
+
+  let gauge = getGauge(Bytes.fromByteArray(crypto.keccak256(event.params._gauge)));
+  gauge.address = event.params._gauge;
   gauge.bunniToken = bunniKey(pool, tickLower, tickUpper);
   gauge.save();
 }
