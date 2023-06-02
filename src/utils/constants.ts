e40817c4ece55f928f56bb2e6db1f547c38ac31a
0xSzeth <0xszeth@gmail.com>
2023-06-02 10:12:55 -0600

    feat: index Warden quests

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,3 +8,5 @@ export let ZERO_ADDR = Address.fromString('0x00000000000000000000000000000000000
 
 export let ONE_BD = BigDecimal.fromString("1");
 export let ONE_INT = BigInt.fromI32(1);
+
+export let WEEK = BigInt.fromI32(604800);
