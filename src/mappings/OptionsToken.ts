74174cd836181d039104c5ec7fa1c73b0f8ec0a1
0xSzeth <0xszeth@gmail.com>
2023-06-23 13:01:12 -0600

    chore: remove old implementation

--- src/mappings/OptionsToken.ts
+++
@@ -1,9 +0,0 @@
-import { BigInt } from "@graphprotocol/graph-ts";
-import { SetParams } from "../../generated/BalancerOracle/BalancerOracle";
-import { getBunni } from "../utils/entities";
-
-export function handleSetParams(event: SetParams): void {
-   let bunni = getBunni();
-   bunni.optionDiscount = BigInt.fromI32(event.params.multiplier);
-   bunni.save();
-}
