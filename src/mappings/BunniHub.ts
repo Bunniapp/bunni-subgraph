7849fa23f9c8d0e7098ab74ab0c35e1835d1038c
0xSzeth <0xszeth@gmail.com>
2023-06-23 16:01:32 -0600

    feat: initialize BunniToken on NewBunni event

--- src/mappings/BunniHub.ts
+++ src/mappings/BunniHub.ts
@@ -1,10 +1,21 @@
 import { Compound, Deposit, NewBunni, PayProtocolFee, SetProtocolFee, Withdraw } from "../types/BunniHub/BunniHub";
+import { getBunniToken } from "../utils/entities";
+import { fetchTokenDecimals, fetchTokenName, fetchTokenSymbol } from "../utils/token";
 
 export function handleCompound(event: Compound): void {}
 
 export function handleDeposit(event: Deposit): void {}
 
-export function handleNewBunni(event: NewBunni): void {}
+export function handleNewBunni(event: NewBunni): void {
+  let bunniToken = getBunniToken(event.params.bunniKeyHash);
+
+  bunniToken.address = event.params.token;
+  bunniToken.decimals = fetchTokenDecimals(event.params.token);
+  bunniToken.name = fetchTokenName(event.params.token);
+  bunniToken.symbol = fetchTokenSymbol(event.params.token);
+
+  bunniToken.save();
+}
 
 export function handlePayProtocolFee(event: PayProtocolFee): void {}
 
