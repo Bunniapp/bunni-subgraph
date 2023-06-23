6d042515b3be28ab174ed72e52ca2134805f02ab
0xSzeth <0xszeth@gmail.com>
2023-06-23 15:36:03 -0600

    chore: init BunniHub mapping file

---
+++ src/mappings/BunniHub.ts
@@ -0,0 +1,13 @@
+import { Compound, Deposit, NewBunni, PayProtocolFee, SetProtocolFee, Withdraw } from "../types/BunniHub/BunniHub";
+
+export function handleCompound(event: Compound): void {}
+
+export function handleDeposit(event: Deposit): void {}
+
+export function handleNewBunni(event: NewBunni): void {}
+
+export function handlePayProtocolFee(event: PayProtocolFee): void {}
+
+export function handleSetProtocolFee(event: SetProtocolFee): void {}
+
+export function handleWithdraw(event: Withdraw): void {}
