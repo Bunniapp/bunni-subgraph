2bd04b9e300af798ac9f983f9879dec98edc9f20
0xSzeth <0xszeth@gmail.com>
2023-06-30 11:44:55 -0600

    feat: index child gauges

---
+++ src/mappings/ChildGauge.ts
@@ -0,0 +1,94 @@
+import { Address, BigDecimal, BigInt, dataSource } from "@graphprotocol/graph-ts";
+import { AddReward, Deposit, Withdraw, UpdateLiquidityLimit, Transfer, NewTokenlessProduction } from "../types/templates/ChildGauge/ChildGauge";
+import { getBunniToken, getGauge, getToken, getUser, getUserPosition } from "../utils/entities";
+import { convertToDecimals } from "../utils/math";
+
+export function handleAddReward(event: AddReward): void {
+  let gauge = getGauge(dataSource.context().getBytes("id"));
+  let token = getToken(event.params.reward_token);
+
+  let rewardTokens = gauge.rewardTokens;
+  if (!rewardTokens.includes(token.id)) {
+    rewardTokens.push(token.id);
+    gauge.rewardTokens = rewardTokens;
+  }
+
+  gauge.save();
+  token.save();
+}
+
+export function handleDeposit(event: Deposit): void {
+  let gauge = getGauge(dataSource.context().getBytes("id"));
+  let userPosition = getUserPosition(getBunniToken(gauge.bunniToken), getUser(event.params._user));
+
+  /// update the gauge total supply
+  let amount = convertToDecimals(event.params._value, gauge.decimals);
+  gauge.totalSupply = gauge.totalSupply.plus(amount);
+
+  /// update the user position gauge balance
+  userPosition.gaugeBalance = userPosition.gaugeBalance.plus(amount);
+
+  gauge.save();
+}
+
+export function handleWithdraw(event: Withdraw): void {
+  let gauge = getGauge(dataSource.context().getBytes("id"));
+  let userPosition = getUserPosition(getBunniToken(gauge.bunniToken), getUser(event.params._user));
+
+  /// update the gauge total supply
+  let amount = convertToDecimals(event.params._value, gauge.decimals);
+  gauge.totalSupply = gauge.totalSupply.minus(amount);
+
+  /// update the user position gauge balance
+  userPosition.gaugeBalance = userPosition.gaugeBalance.minus(amount);
+
+  gauge.save();
+}
+
+export function handleUpdateLiquidityLimit(event: UpdateLiquidityLimit): void {
+  let gauge = getGauge(dataSource.context().getBytes("id"));
+  let userPosition = getUserPosition(getBunniToken(gauge.bunniToken), getUser(event.params._user));
+
+  /// update the gauge working supply
+  gauge.workingSupply = convertToDecimals(event.params._working_supply, gauge.decimals);
+
+  /// update the user position working balance
+  userPosition.workingBalance = convertToDecimals(event.params._working_balance, gauge.decimals);
+
+  gauge.save();
+  userPosition.save();
+}
+
+export function handleTransfer(event: Transfer): void {
+  /// ignore minting and burning events
+  if (event.params._from != Address.zero() && event.params._to != Address.zero()) {
+    let gauge = getGauge(dataSource.context().getBytes("id"));
+    let fromPosition = getUserPosition(getBunniToken(gauge.bunniToken), getUser(event.params._from));
+    let toPosition = getUserPosition(getBunniToken(gauge.bunniToken), getUser(event.params._to));
+    let amount = convertToDecimals(event.params._value, gauge.decimals);
+
+    /// update sender user position
+    fromPosition.gaugeBalance = fromPosition.gaugeBalance.minus(amount);
+
+    /// update recipient user position, accounting for the cost basis of the amount transferred
+    let toOldTotalBalance = toPosition.balance.plus(toPosition.gaugeBalance);
+    let toNewTotalBalance = toPosition.balance.plus(toPosition.gaugeBalance).plus(amount);
+
+    let token0CostBasis = toPosition.token0CostBasisPerShare.times(toOldTotalBalance).plus(fromPosition.token0CostBasisPerShare.times(amount));
+    let token1CostBasis = toPosition.token1CostBasisPerShare.times(toOldTotalBalance).plus(fromPosition.token1CostBasisPerShare.times(amount));
+    if (toNewTotalBalance.gt(BigDecimal.zero())) {
+      toPosition.token0CostBasisPerShare = token0CostBasis.div(toNewTotalBalance);
+      toPosition.token1CostBasisPerShare = token1CostBasis.div(toNewTotalBalance);
+    }
+    toPosition.gaugeBalance = toPosition.gaugeBalance.plus(amount);
+
+    fromPosition.save();
+    toPosition.save();
+  }
+}
+
+export function handleNewTokenlessProduction(event: NewTokenlessProduction): void {
+  let gauge = getGauge(dataSource.context().getBytes("id"));
+  gauge.tokenlessProduction = BigInt.fromI32(event.params.new_tokenless_production);
+  gauge.save();
+}
--- src/mappings/LiquidityGaugeFactory.ts
+++ src/mappings/LiquidityGaugeFactory.ts
@@ -1,9 +1,11 @@
 import { Bytes, DataSourceContext, crypto, dataSource } from "@graphprotocol/graph-ts";
 import { BunniToken } from "../types/LiquidityGaugeFactory/BunniToken";
 import { LiquidityGauge } from "../types/LiquidityGaugeFactory/LiquidityGauge";
+import { ChildGauge } from "../types/ChildGaugeFactory/ChildGauge";
 import { GaugeCreated } from "../types/LiquidityGaugeFactory/LiquidityGaugeFactory";
+import { DeployedGauge as DeployedChildGauge } from "../types/ChildGaugeFactory/ChildGaugeFactory";
 import { DeployedGauge as DeployedRootGauge } from "../types/RootGaugeFactory/RootGaugeFactory";
-import { LiquidityGauge as LiquidityGaugeTemplate, RootGauge as RootGaugeTemplate } from "../types/templates";
+import { LiquidityGauge as LiquidityGaugeTemplate, ChildGauge as ChildGaugeTemplate, RootGauge as RootGaugeTemplate } from "../types/templates";
 
 import { CHAIN_ID } from "../utils/constants";
 import { getBunniToken, getGauge } from "../utils/entities";
@@ -35,6 +37,33 @@ export function handleGaugeCreated(event: GaugeCreated): void {
   let liquidityGaugeContext = new DataSourceContext();
   liquidityGaugeContext.setBytes("id", gauge.id);
   LiquidityGaugeTemplate.createWithContext(event.params.gauge, liquidityGaugeContext);
+}
+
+export function handleDeployedChildGauge(event: DeployedChildGauge): void {
+  let gaugeContract = ChildGauge.bind(event.params._gauge);
+  let bunniTokenContract = BunniToken.bind(gaugeContract.lp_token());
+
+  let bunniToken = getBunniToken(bunniKey(bunniTokenContract.pool(), bunniTokenContract.tickLower(), bunniTokenContract.tickUpper()));
+  let gauge = getGauge(Bytes.fromByteArray(crypto.keccak256(event.params._gauge)));
+
+  gauge.address = event.params._gauge;
+  gauge.decimals = fetchTokenDecimals(event.params._gauge);
+  gauge.name = fetchTokenName(event.params._gauge);
+  gauge.symbol = fetchTokenSymbol(event.params._gauge);
+
+  gauge.bunniToken = bunniToken.id;
+
+  let chain = CHAIN_ID.get(dataSource.network());
+  if (chain !== null) gauge.chain = chain;
+
+  bunniToken.gauge = gauge.id;
+
+  bunniToken.save();
+  gauge.save();
+
+  let childGaugeContext = new DataSourceContext();
+  childGaugeContext.setBytes("id", gauge.id);
+  ChildGaugeTemplate.createWithContext(event.params._gauge, childGaugeContext);
 }
 
 export function handleDeployedRootGauge(event: DeployedRootGauge): void {
