2a90bf6ea8d9edca931c7491a37f7499b66860a0
0xSzeth <0xszeth@gmail.com>
2023-07-05 10:50:47 -0600

    feat: index claimedRewards for child gauges

--- src/mappings/LiquidityGaugeFactory.ts
+++ src/mappings/LiquidityGaugeFactory.ts
@@ -1,16 +1,17 @@
-import { Bytes, DataSourceContext, crypto, dataSource } from "@graphprotocol/graph-ts";
+import { BigInt, Bytes, DataSourceContext, crypto, dataSource } from "@graphprotocol/graph-ts";
 import { BunniToken } from "../types/LiquidityGaugeFactory/BunniToken";
 import { LiquidityGauge } from "../types/LiquidityGaugeFactory/LiquidityGauge";
 import { ChildGauge } from "../types/ChildGaugeFactory/ChildGauge";
 import { GaugeCreated } from "../types/LiquidityGaugeFactory/LiquidityGaugeFactory";
-import { DeployedGauge as DeployedChildGauge } from "../types/ChildGaugeFactory/ChildGaugeFactory";
+import { DeployedGauge as DeployedChildGauge, Minted } from "../types/ChildGaugeFactory/ChildGaugeFactory";
 import { DeployedGauge as DeployedRootGauge } from "../types/RootGaugeFactory/RootGaugeFactory";
 import { LiquidityGauge as LiquidityGaugeTemplate, ChildGauge as ChildGaugeTemplate, RootGauge as RootGaugeTemplate } from "../types/templates";
 
 import { CHAIN_ID } from "../utils/constants";
-import { getBunniToken, getGauge } from "../utils/entities";
+import { getBunniToken, getGauge, getUser, getUserPosition } from "../utils/entities";
 import { bunniKey } from "../utils/helpers";
 import { fetchTokenDecimals, fetchTokenName, fetchTokenSymbol } from "../utils/token";
+import { convertToDecimals } from "../utils/math";
 
 export function handleGaugeCreated(event: GaugeCreated): void {
   let gaugeContract = LiquidityGauge.bind(event.params.gauge);
@@ -76,4 +77,27 @@ export function handleDeployedRootGauge(event: DeployedRootGauge): void {
   let rootGaugeContext = new DataSourceContext();
   rootGaugeContext.setBytes("id", gauge.id);
   RootGaugeTemplate.createWithContext(event.params._gauge, rootGaugeContext);
+}
+
+export function handleMinted(event: Minted): void {
+  let gauge = getGauge(Bytes.fromByteArray(crypto.keccak256(event.params._gauge)));
+  let bunniToken = getBunniToken(gauge.bunniToken);
+
+  let user = getUser(event.params._user);
+  let userPosition = getUserPosition(bunniToken, user);
+
+  /// reset aggregates until new amounts calculated
+  let oldAmount = userPosition.claimedRewards;
+  user.claimedRewards = user.claimedRewards.minus(oldAmount);
+  gauge.claimedRewards = gauge.claimedRewards.minus(oldAmount);
+
+  /// update with new amounts
+  let newAmount = convertToDecimals(event.params._new_total, BigInt.fromI32(18));
+  gauge.claimedRewards = gauge.claimedRewards.plus(newAmount);
+  user.claimedRewards = user.claimedRewards.plus(newAmount);
+  userPosition.claimedRewards = newAmount;
+
+  gauge.save();
+  user.save();
+  userPosition.save();
 }
